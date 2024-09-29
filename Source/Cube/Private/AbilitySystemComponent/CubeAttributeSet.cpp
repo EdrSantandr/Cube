dd3ebb107e82db1232edf75badc46f43091ab0f3
Eder Santander <eder.santander.123@gmail.com>
2024-09-29 15:22:32 -0500

    Added stamina and maxstamina attributes

--- Source/Cube/Private/AbilitySystemComponent/CubeAttributeSet.cpp
+++ Source/Cube/Private/AbilitySystemComponent/CubeAttributeSet.cpp
@@ -2,4 +2,28 @@
 
 
 #include "AbilitySystemComponent/CubeAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+
+UCubeAttributeSet::UCubeAttributeSet()
+{
+}
+
+void UCubeAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCubeAttributeSet, Stamina, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UCubeAttributeSet, MaxStamina, COND_None, REPNOTIFY_Always);
+}
+
+void UCubeAttributeSet::OnRep_Stamina(const FGameplayAttributeData& OldStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCubeAttributeSet, Stamina, OldStamina);
+}
+
+void UCubeAttributeSet::OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UCubeAttributeSet, MaxStamina, OldMaxStamina);
+}
 
--- Source/Cube/Public/AbilitySystemComponent/CubeAttributeSet.h
+++ Source/Cube/Public/AbilitySystemComponent/CubeAttributeSet.h
@@ -13,5 +13,19 @@ UCLASS()
 class CUBE_API UCubeAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	UCubeAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Stamina, Category = "MovementAttributes")
+	FGameplayAttributeData Stamina;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxStamina, Category = "MovementAttributes")
+	FGameplayAttributeData MaxStamina;
+
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldStamina) const;
+
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldMaxStamina) const;
 };
