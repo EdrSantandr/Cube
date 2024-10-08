6a617db9e18b1f051766cea9b983dd0619c6e431
Eder Santander <eder.santander.123@gmail.com>
2024-10-08 10:11:38 -0500

    Setup the apply effect on actor

--- Source/Cube/Private/Actor/CubeEffectActor.cpp
+++ Source/Cube/Private/Actor/CubeEffectActor.cpp
@@ -2,9 +2,8 @@
 
 
 #include "Actor/CubeEffectActor.h"
-
-#include "AbilitySystemInterface.h"
-#include "AbilitySystemComponent/CubeAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemBlueprintLibrary.h"
 #include "Components/BoxComponent.h"
 
 ACubeEffectActor::ACubeEffectActor()
@@ -27,17 +26,35 @@ void ACubeEffectActor::BeginPlay()
 void ACubeEffectActor::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	UE_LOG(LogTemp, Warning, TEXT("BEGIN OVERLAP"));
-	if (IAbilitySystemInterface* AscInterface = Cast<IAbilitySystemInterface>(OtherActor))
+	switch (EffectType)
 	{
-		const UCubeAttributeSet* CubeAttributeSet = Cast<UCubeAttributeSet>(AscInterface->GetAbilitySystemComponent()->GetAttributeSet(UCubeAttributeSet::StaticClass()));
-		UCubeAttributeSet* MutableAttributeSet = const_cast<UCubeAttributeSet*>(CubeAttributeSet);
-		//MutableAttributeSet->SetMovementTime(CubeAttributeSet->GetMovementTime() - 0.2f);
-		MutableAttributeSet->SetStamina(CubeAttributeSet->GetStamina() + 25.f);
-		Destroy();
+		case EAppliedAbilityEffectType::Instant:
+			ApplyEffectToTarget(OtherActor, InstantGameplayEffect);
+
+		case EAppliedAbilityEffectType::Duration:
+			ApplyEffectToTarget(OtherActor, DurationGameplayEffect);
+
+		case EAppliedAbilityEffectType::Infinite:
+			ApplyEffectToTarget(OtherActor, InfiniteGameplayEffect);
+		default:
+			return;
 	}
 }
 
 void ACubeEffectActor::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	UE_LOG(LogTemp, Warning, TEXT("END OVERLAP"));
+}
+
+void ACubeEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	if (UAbilitySystemComponent* TargetAsc = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target))
+	{
+		check(GameplayEffectClass);
+		FGameplayEffectContextHandle EffectContextHandle = TargetAsc->MakeEffectContext();
+		EffectContextHandle.AddSourceObject(this);
+		//todo: level should be set
+		FGameplayEffectSpecHandle EffectSpecHandle = TargetAsc->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+		TargetAsc->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	}
 }
--- Source/Cube/Public/Actor/CubeEffectActor.h
+++ Source/Cube/Public/Actor/CubeEffectActor.h
@@ -6,7 +6,16 @@
 #include "GameFramework/Actor.h"
 #include "CubeEffectActor.generated.h"
 
+class UGameplayEffect;
 class UBoxComponent;
+
+UENUM()
+enum class EAppliedAbilityEffectType : uint8
+{
+	Instant,
+	Duration,
+	Infinite
+};
 
 UCLASS()
 class CUBE_API ACubeEffectActor : public AActor
@@ -24,6 +33,21 @@ protected:
 
 	UFUNCTION()
 	virtual void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditDefaultsOnly, Category="Effects")
+	EAppliedAbilityEffectType EffectType = EAppliedAbilityEffectType::Instant;
+	
+	UPROPERTY(EditAnywhere, Category="InstantEffect")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffect;
+
+	UPROPERTY(EditAnywhere, Category="DurationEffect")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffect;
+
+	UPROPERTY(EditAnywhere, Category="InfiniteEffect")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffect;
+	
+	UFUNCTION(BlueprintCallable, Category="Effects")
+	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
 private:
 	UPROPERTY(VisibleAnywhere)
